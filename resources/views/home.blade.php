41d6042882eccc1acc347f13d202d3731125fdd4
AlexsandroD <adrdra88@gmail.com>
2022-02-04 17:23:18 +0100

    fine esercizio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
 <body>
-    <ul>
+    <div class="container">
         @foreach ($movies as $movie )
-            <li>{{ $movie->title }}</li>
+            <div class="card">
+                <h1 class="title">{{ $movie -> title }}</h1>
+                <ul>
+                    <li>Titolo originale:{{ $movie -> original_title }}</li>
+                    <li>Nationalitá{{ $movie -> nationality}}</li>
+                    <li>Data primo screen:{{ $movie -> date}}</li>
+                    <li>Voto:{{ $movie -> vote }}</li>
+                </ul>
+            </div>
         @endforeach
-    </ul>
+    </div>
 </body>
 </html>
